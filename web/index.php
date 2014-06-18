7975b58a2ceab9ee23fe4e1a453c103166048b89
marcoschamp <marcos.campodonico@gointegro.com>
2014-06-18 18:57:55 +0000

    print cards

--- web/index.php
+++ web/index.php
@@ -17,17 +17,30 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/login/', function (Request $request) use ($app) {
 
-	$encodeData = $request->query->get('data');
-	$salt = $app['config']['secret'];
-	
-    $decodeData = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $salt, base64_decode($encodeData), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
-    
-    $userParams = json_decode($decodeData); 
 
-	return $app['twig']->render('index.twig', array(
-        'user' => $userParams,
-    ));
+	$client_url =  $_SERVER['HTTP_HOST'];
+	if( $app['config']['client_url'][$client_url]['url'] == $client_url ) {
 
+		$encodeData = $request->query->get('data');
+		$salt = $app['config']['secret'];
+
+                $decodeData = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $salt, base64_decode($encodeData), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
+
+                $userParams = json_decode($decodeData);
+		$pathImageCardFront = $app['config']['client_url'][$client_url]['path_image_card_front'];
+		$pathImageCardBack = $app['config']['client_url'][$client_url]['path_image_card_back'];
+
+
+		return $app['twig']->render('index.twig', array(
+		       'user' => $userParams,
+		       'pathImageCardFront' => $pathImageCardFront,
+          	       'pathImageCardBack' => $pathImageCardBack,			
+                ));
+	} else {
+		 return $app['twig']->render('error.twig', array(
+                       'message' => "sitio web no encontrado",
+                ));
+	}
 });
 
 
